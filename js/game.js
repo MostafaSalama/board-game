6b0accff61d9784651f6a2a0699bc64d8dbeacb3
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-03 10:18:22 +0200

    refactor BoardBox to be in its own file

--- js/game.js
+++ js/game.js
@@ -64,23 +64,7 @@ const actionTypes = {
 	DEFEND: 'DEFEND',
 	ATTACK: 'ATTACK',
 };
-class BoardBox {
-	/**
-	 *
-	 * @param position {ItemPosition}
-	 * @param element {HTMLElement}
-	 * @param index {number}
-	 */
-	constructor(position, element, index) {
-		this.position = position;
-		this.element = element;
-		this.index = index;
-		this.isEmpty = true;
-		this.filledWith = null;
-		this.weapon = null;
-		this.player = null;
-	}
-}
+
 
 class Weapon {
 	/**

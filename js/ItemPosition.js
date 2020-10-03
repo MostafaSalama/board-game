5e481d2380e21723930315cc9dc19ccafd627491
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-03 10:17:18 +0200

    update ItemPosition Class to be in a seperate file

---
+++ js/ItemPosition.js
@@ -0,0 +1,15 @@
+
+/**
+ * store the position of an item using its x and y
+ */
+class ItemPosition {
+    /**
+     *
+     * @param x {number} x-axis position (row)
+     * @param y {number} y-axis position (column)
+     */
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- js/game.js
+++ js/game.js
@@ -64,20 +64,6 @@ const actionTypes = {
 	DEFEND: 'DEFEND',
 	ATTACK: 'ATTACK',
 };
-/**
- * store the position of an item using its x and y
- */
-class ItemPosition {
-	/**
-	 *
-	 * @param x {number} x-axis position (row)
-	 * @param y {number} y-axis position (column)
-	 */
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
-}
 class BoardBox {
 	/**
 	 *
